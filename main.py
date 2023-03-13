cb05cbf6434b480a0f2393114cbf12a94c049dd8
EvaTezeta <118466414+EvaTezeta@users.noreply.github.com>
2023-03-13 09:18:45 +0000

    Common interests and faculties

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import csv
 import random
 
-# Create empty list
+# A list to add all the participants
 participants = []
 
 # Return the content of the file as list of dictionaries
@@ -18,7 +18,7 @@ random.shuffle(participants)
 initial_group_size = 0
 possible_group_sizes = [2, 3, 4, 5]
 
-#A list for the groups
+# A list for the groups
 groups = []
 
 while participants:
@@ -44,10 +44,6 @@ while participants:
     # Add the group to the list of groups
     groups.append(group)
 
-# Give an explanation what the lists are
-print("""Thank you for using chatCOFFEE!
-The lists below are the groups for today's chat.\n""")
-
 # Print the groups
 for i, group in enumerate(groups):
     print(f"Group {i} ({len(group)} participants):")
@@ -55,7 +51,7 @@ for i, group in enumerate(groups):
         print(f"- {participant['name']} ({participant['email']})")
     print()
 
-#An image to add to the text file
+# An image to added to the message
 art = r""" 
           )    ) (      
          (    (  )
@@ -78,19 +74,47 @@ with open("conversation_starters.txt", "r") as file:
 # Create function to generate messages
 def generate_messages_and_save_files(groups):
     for i in range(len(groups)):
+
         # Get the list of names, faculties and interests of the group members
         group = groups[i]
         names = [member["name"] for member in group]
         faculties = [member["faculty"] for member in group]
         interests = [member["interest"] for member in group]
 
+        #Check if the participants are from the same faculties
+        def common_faculties():
+            x = 0
+            for one in faculties:
+                for another in faculties:
+                    if one == another:
+                        x += 1
+                        if x > len(groups[i]):
+                            sentence = "Some of you are from the same faculty!"
+                        else:
+                            sentence = "You are all from different faculties."
+            return sentence
+
+        #Check if the participants have a common interest
+        def common_interests():
+            y = 0
+            for one_i in interests:
+                for another_i in interests:
+                    if one_i == another_i:
+                        y += 1
+                        if y > len(groups[i]):
+                            sentence_i = "Some of you have common interests!"
+                        else:
+                            sentence_i = "You all have different interests."
+            return sentence_i
+
         # Randomly choose a conversation starter from the list
         random_starter = random.choice(conversation_starters).strip()
 
         # Generate message with names, faculties and interests
         group_message = (
-            f"Hello {' & '.join(names)}! You have been matched to group {i} for today's coffee talk.\n\nYou are in the faculties of: {' & '.join(faculties)}.\n\nYour interests are: {' & '.join(interests)}.\n\nHere's a question to get talking: {random_starter}\n\nEnjoy your chat with a nice cup of coffee!\n{art}"
+            f"""Hello {' & '.join(names)}! You have been matched to group {i} for today's coffee talk. Here is some information that might be interesting for you.\n\n{common_faculties()} \nThe faculties you are from are: {' & '.join(faculties)}.\n\n{common_interests()} Your interests are: {' & '.join(interests)}. \n\nHere's a question to get talking: {random_starter}\n\nEnjoy your chat with a nice cup of coffee!\n{art} """
         )
+
         print(group_message)
 
         # Create new text files for each group with the random starter
